9087ed68416b3ceb69c95bff205158715daa22fe
Jake Zeisel <jacobzeisel@gmail.com>
2023-12-09 19:31:36 -0600

    add tempo estimator class

---
+++ Source/Tempo_Estimator.h
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    Tempo_Estimator.h
+    Created: 9 Dec 2023 7:30:22pm
+    Author:  Jacob Zeisel
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+using namespace juce;
+
+class Tempo_Estimator {
+public:
+private:
+};
