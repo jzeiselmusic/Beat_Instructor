09064f6ae05ef1919ca13c796e18afe4985f4205
Jake Zeisel <jacobzeisel@gmail.com>
2023-12-10 15:03:22 -0600

    add onset detector and utils

--- Source/Onset_Detector.h
+++ Source/Onset_Detector.h
@@ -12,8 +12,58 @@
 
 #include <JuceHeader.h>
 #include "Gamma/Gamma/DFT.h"
+#include "utils.h"
 
 class Onset_Detector {
 public:
+    Onset_Detector(double sample_rate, int size) {
+        OD_sampleRate = sample_rate;
+        OD_size = size;
+        binsMagsNew = (float*)malloc((size_t)size);
+        binsMagsOld = (float*)malloc((size_t)size);
+        stft = gam::STFT(size, size/4, 0, 
+                gam::WindowType::HAMMING, 
+                gam::SpectralType::COMPLEX);
+    }
+
+    ~Onset_Detector() {
+        free(binsMagsNew);
+        free(binsMagsOld);
+    }
+
+    /* to use this class: process every sample */
+    /* if return true when processing sample, then grab the latest avail sum */
+
+    bool process(double input) {
+        isSpectrumReady = stft((float)input);
+
+        if (isSpectrumReady) {
+            for (int i = 0; i < stft.numBins(); ++i) {
+                // store each bin as the L1 norm of the complex FFT
+                binsMagsOld[i] = binsMagsNew[i];
+                binsMagsNew[i] = abs(stft.bin(i).real()) + abs(stft.bin(i).imag());
+            }
+        }
+        return isSpectrumReady;
+    }
+
+    float* getLatestSpectrum() {
+        return binsMagsNew;
+    }
+
+    float onsetDectionSum() {
+        float total = 0.0;
+        for (int i = 0; i < stft.numBins(); ++i) {
+            total += halfWaveRect(binsMagsNew[i] - binsMagsOld[i]);
+        }
+        return total;
+    }
+
 private:
-}
+    bool isSpectrumReady;
+    int OD_size;
+    double OD_sampleRate;
+    gam::STFT stft;
+    float* binsMagsNew;
+    float* binsMagsOld;
+};
